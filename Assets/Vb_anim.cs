ddb1313c617f8eb0b9e0737d3f16427f323b5a5b
TheSevaOne <alfaseva@yandex.com>
2018-12-12 02:13:53 +0300

    project add

---
+++ Assets/Vb_anim.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Vb_anim : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("vbutton");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("Cube_Animation");
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Button released");
+    }
+}
