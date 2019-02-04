a6f0da6f130653e4149105997d15aa6db71e91ac
TheSevaOne <alfaseva@yandex.com>
2019-02-05 01:19:15 +0300

    update

---
+++ AR/Assets/CameraFocusController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+        var vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnVuforiaStarted()
+    {
+       Vuforia.CameraDevice.Instance.SetFocusMode(
+         Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused) // resumed
+        {
+            // Set again autofocus mode when app is resumed
+           Vuforia.CameraDevice.Instance.SetFocusMode(
+              Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
---
+++ AR/Assets/Control.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using Vuforia;
+public class Control : MonoBehaviour   {
+    private Rigidbody rb;
+    [SerializeField]
+    private float speed=2f;
+	void Start () {
+      
+        rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+		float y = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector3 moverment = new Vector3(x, 0, y);
+        rb.velocity = moverment * speed;
+        if (x != 0 && y != 0)
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x,Mathf.Atan2(x,y)+Mathf.Rad2Deg, transform.eulerAngles.z );
+
+    }
+   
+}
+public class PhoneCamera : MonoBehaviour
+{
+    public void OnCameraChangeMode()
+    {
+        Vuforia.CameraDevice.CameraDirection currentDir = Vuforia.CameraDevice.Instance.GetCameraDirection();
+
+
+        RestartCamera(Vuforia.CameraDevice.CameraDirection.CAMERA_FRONT);
+
+
+
+    }
+
+
+  
+    private void RestartCamera(Vuforia.CameraDevice.CameraDirection newDir)
+    {
+        Vuforia.CameraDevice.Instance.Stop();
+        Vuforia.CameraDevice.Instance.Deinit();
+        Vuforia.CameraDevice.Instance.Init(newDir);
+        Vuforia.CameraDevice.Instance.Start();
+    }
+}
+
+
+
+
